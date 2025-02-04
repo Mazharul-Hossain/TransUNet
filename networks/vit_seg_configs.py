8d904ffe5f5b03c7965785069895e844ea61f28c
Mazhar <2561458+Mazharul-Hossain@users.noreply.github.com>
2025-02-04 11:42:40 -0600

    UAV_HSI_Crop 13th Exp

--- networks/vit_seg_configs.py
+++ networks/vit_seg_configs.py
@@ -55,8 +55,6 @@ def get_r50_b16_config():
     config.pretrained_path = (
         "/project/mhssain9/model/vit_checkpoint/imagenet21k/R50+ViT-B_16.npz"
     )
-    config["pretrained_path_alt1"] = "D:/Downloads/project/imagenet21k+imagenet2012/R50+ViT-B_16.npz"
-    
 
     config.decoder_channels = (256, 128, 64, 16)
     config.skip_channels = [512, 256, 64, 16]
